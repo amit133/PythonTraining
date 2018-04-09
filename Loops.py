d939a8061fc9389cc1e73cfd3136dbc8d4c1a93d
amit <amit.8k@gmail.com>
2018-04-09 12:58:29 +0530

    loop statements in python

---
+++ Loops.py
@@ -0,0 +1,16 @@
+#Loops
+primes = [2,3,5,7]
+
+#for loop
+for prime in primes:
+    print(prime, end=' ')
+
+print()
+
+#while loop
+counter = 0
+while counter < len(primes):
+    print(primes[counter], end=' ')
+    counter += 1;
+else:
+    print("\nAll list elements printed")
