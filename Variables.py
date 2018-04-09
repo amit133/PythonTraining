015c1446e1ea50bef0385b6f2a0084f55bd00b8d
amit <amit.8k@gmail.com>
2018-04-09 12:55:19 +0530

    declare variables

---
+++ Variables.py
@@ -0,0 +1,15 @@
+# variable types
+myint = 2
+print(myint)
+
+myfloat = 2.1
+print(myfloat)
+
+mystr1 = 'string under single quote'
+print(mystr1)
+
+mystr2 = "string under double quote"
+print(mystr2)
+
+print('{1} {0}'.format('one', 'two'))
+
