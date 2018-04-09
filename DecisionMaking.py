90a6e7587f92b112dfa3887c0dd7d305de46a8d9
amit <amit.8k@gmail.com>
2018-04-09 12:57:57 +0530

    if statements in python

---
+++ DecisionMaking.py
@@ -0,0 +1,11 @@
+#Decision making
+if True:
+    print ("hard coded True")
+
+if (True):
+    print("Bracketed if")
+
+name = ['amit', 'kumar']
+fname = 'amit'
+if fname in name:
+    print("You are '{0} {1}'".format(name[0], name[1]))
