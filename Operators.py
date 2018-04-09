7d69581565a9526970fd29e44c559e41c8d7a8da
amit <amit.8k@gmail.com>
2018-04-09 12:56:24 +0530

    python operators

---
+++ Operators.py
@@ -0,0 +1,10 @@
+#Operators
+squared = 5 ** 2
+print('square of 5 is {0}'.format(squared))
+
+#plus operator and formatted print
+helloworld = 'hello' + " " + 'world'
+print('{}\n'.format(helloworld))
+
+manyhellos = 'hello' * 5
+print(manyhellos)
