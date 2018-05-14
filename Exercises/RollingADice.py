f0a0bdcd5e63bbf3d1b3bf50ae15574e784a8a3b
amit <amit.8k@gmail.com>
2018-05-14 11:22:15 +0530

    Generating non-repeating random numbers from a given set of numbers

--- Exercises/RollingADice.py
+++ Exercises/RollingADice.py
@@ -3,23 +3,46 @@ import random
 from playsound import playsound
 from PIL import Image
 
-engine = pyttsx.init()
+#engine = pyttsx.init()
+#
+#engine.say('Lets roll two dice')
+#
+#engine.runAndWait()
+#
+#number1 = random.randint(1, 90)
+#number2 = random.randint(1, 6)
+#
+## play a sound file
+#playsound('./sound/RollTwoDice.mp3')
+#
+#rollResult = 'You rolled ' + str(number1) + ' and ' + str(number2)
+#engine.say(rollResult)
+#print(rollResult)
+#engine.runAndWait()
 
-engine.say('Lets roll two dice')
+#img = Image.open('./images/200w.webp')
+#img.show()
 
-engine.runAndWait()
 
-number1 = random.randint(1, 6)
-number2 = random.randint(1, 6)
+myrange = list(range(1, 6+1))
+# get a random number
+# if that number was drawn before, get another random number till it doesn't match
 
-# play a sound file
-playsound('./sound/RollTwoDice.mp3')
+play = True
+while play and len(myrange) > 1:
+	a = random.choice(myrange)
+	#print(a)
+	myrange.remove(a)
+	while True:
+		toContinue = input('Do you wish to continue the game (Y/N)? ').lower()
+		print(a)
+		if toContinue == 'y' or toContinue == "":
+			play = True
+			break
+		elif toContinue == 'n':
+			play = False
+			break
+		else:
+			toContinue = input('Enter only Y or N. \n Do you wish to continue the game (Y/N)? ').lower()
 
-rollResult = 'You rolled ' + str(number1) + ' and ' + str(number2)
-engine.say(rollResult)
-print(rollResult)
-engine.runAndWait()
-
-img = Image.open('./images/200w.webp')
-img.show()
-
+print('And the last number is {0}'.format(myrange[0]))			
